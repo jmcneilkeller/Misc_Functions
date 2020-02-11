714985d98a0c99d1fcfa1f73e1917274b2877157
Jonathan Keller <jmcneilkeller@gmail.com>
2020-02-11 15:54:39 -0500

    Initial commit. Added RFECV helper function.

---
+++ feature_selection.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from sklearn.feature_selection import RFECV
+import pprint
+
+def rfe(estimator,step,cv,scoring,X_train,y_train):
+    # Instantiate Recursive Feature Elimination
+    selector = RFECV(estimator=estimator, step=step, cv=cv, scoring=scoring)
+    selector.fit(X_train,y_train)
+    
+    selected_columns = X_train.columns[selector.support_]
+    removed_columns = X_train.columns[~selector.support_]
+    print('*'*20+'SELECTED'+'*'*19)
+    pprint.pprint(list(selected_columns))
+    print('\n'+'*'*20+'REMOVED'+'*'*20)
+    pprint.pprint(list(removed_columns))
+    return selected_columns
