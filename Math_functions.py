f636784ea68ee056c51d1f87ef72e6437f548659
Jonathan Keller <jmcneilkeller@gmail.com>
2020-09-01 09:52:50 -0400

    Updated statistical test.

--- Math_functions.py
+++ Math_functions.py
@@ -26,30 +26,6 @@ def overlap_superiority(group1, group2, n=1000):
 
     return overlap, superiority
 
-
-def Cohen_d(group1, group2):
-
-    # Compute Cohen's d.
-
-    # group1: Series or NumPy array
-    # group2: Series or NumPy array
-
-    # returns a floating point number
-
-    diff = group1.mean() - group2.mean()
-
-    n1, n2 = len(group1), len(group2)
-    var1 = group1.var()
-    var2 = group2.var()
-
-    # Calculate the pooled threshold as shown earlier
-    pooled_var = (n1 * var1 + n2 * var2) / (n1 + n2)
-
-    # Calculate Cohen's d statistic
-    d = diff / np.sqrt(pooled_var)
-
-    return d
-
 def exp_pdf(mu, x):
     decay_rate = 1 / mu
     return decay_rate * np.exp(-decay_rate * x)
